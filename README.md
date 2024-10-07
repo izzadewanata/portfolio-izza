58b625f0fa9f55fac8f2fd8fee1120b6d528f96b
Izza Sinatrya <izza.career@gmail.com>
2024-10-07 23:36:56 +1100

    update-#3

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Basis: https://www.youtube.com/watch?v=_xkSvufmjEs&t=23s
 Design document: https://adobe.ly/2GAgF93
-Pause the video at: https://youtu.be/_xkSvufmjEs?t=2233
+Pause the video at: https://youtu.be/_xkSvufmjEs?t=4130
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -21,9 +21,9 @@ export default function Services() {
                 <h3>WordPress</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             </div>
-
-            <a href="#work" className="btn">My Work</a>
         </div>
+
+        <a href="#work" className="btn">My Work</a>
     </section>
   )
 }
