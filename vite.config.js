61503e59cec0de603e941f0431cf8f6b7aa07260
Izza Sinatrya <izza.career@gmail.com>
2026-06-22 23:19:34 +0900

    New file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio-izza/"
+  build: {
+    outDir: 'docs',  // ← This tells Vite to build to /docs folder
+    emptyOutDir: true
+  },
+  base: '/portfolio-izza/'
 })
